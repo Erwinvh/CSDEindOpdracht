1c8a38aba503bcc461b03395c8e919cddf5e07c1
Erwin <Erwinvanhunnik@gmail.com>
2021-01-15 17:04:09 +0100

    [Addition/Alteration] buttons work semi well
    added 2 fragments

--- app/src/main/java/com/example/csdeindopdracht/MainActivity.java
+++ app/src/main/java/com/example/csdeindopdracht/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.csdeindopdracht;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.example.csdeindopdracht.fragments.BaseMapFragment;
 import com.example.csdeindopdracht.fragments.CharacterFragment;
+import com.example.csdeindopdracht.fragments.raceFragment;
+import com.example.csdeindopdracht.fragments.trainingFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     // Current and all fragments.
     private Fragments currentFragment = Fragments.BASE_MAP;
+    private FloatingActionButton toCharacterButton;
+    private FloatingActionButton trainingButton;
+    private  FloatingActionButton racingButton;
+
+
     private enum Fragments {
         BASE_MAP,
         CHARACTER,
@@ -26,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //left button
-        FloatingActionButton toCharacterButton = findViewById(R.id.button_character);
+        toCharacterButton = findViewById(R.id.button_character);
         toCharacterButton.setOnClickListener(v -> {
             if (!currentFragment.equals(Fragments.CHARACTER)) {
                 //TODO: register previous fragment?
@@ -34,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                         .replace(R.id.fragmentcontainer, new CharacterFragment())
                         .commit();
                 currentFragment = Fragments.CHARACTER;
+                resetButtons();
                 toCharacterButton.setImageResource(R.drawable.stop_foreground);
                 //TODO: change floatingbutton to globe
             }else{
@@ -42,19 +49,20 @@ public class MainActivity extends AppCompatActivity {
                         .replace(R.id.fragmentcontainer, new BaseMapFragment())
                         .commit();
                 currentFragment = Fragments.BASE_MAP;
-                toCharacterButton.setImageResource(R.drawable.ic_launcher_foreground);
+                resetButtons();
             }
         });
 
         //Middle button
-        FloatingActionButton trainingButton = findViewById(R.id.button_training);
+        trainingButton = findViewById(R.id.button_training);
         trainingButton.setOnClickListener(v -> {
             if (!currentFragment.equals(Fragments.TRAINING)) {
                 //TODO: register previous fragment?
                 getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragmentcontainer, new CharacterFragment()) // TODO change fragment
+                        .replace(R.id.fragmentcontainer, new trainingFragment()) // TODO change fragment
                         .commit();
                 currentFragment = Fragments.TRAINING;
+                resetButtons();
                 trainingButton.setImageResource(R.drawable.stop_foreground);
             }else{
                 //TODO: turn back to previous fragment or map fragment
@@ -62,19 +70,20 @@ public class MainActivity extends AppCompatActivity {
                         .replace(R.id.fragmentcontainer, new BaseMapFragment())
                         .commit();
                 currentFragment = Fragments.BASE_MAP;
-                trainingButton.setImageResource(R.drawable.training_foreground);
+                resetButtons();
             }
         });
 
         //right button
-        FloatingActionButton racingButton = findViewById(R.id.button_race);
+        racingButton = findViewById(R.id.button_race);
         racingButton.setOnClickListener(v -> {
             if (!currentFragment.equals(Fragments.RACE)){
                 //TODO: register previous fragment?
                 getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragmentcontainer, new CharacterFragment()) // TODO change fragment
+                        .replace(R.id.fragmentcontainer, new raceFragment()) // TODO change fragment
                         .commit();
                 currentFragment = Fragments.RACE;
+                resetButtons();
                 racingButton.setImageResource(R.drawable.stop_foreground);
 
             }else{
@@ -83,8 +92,8 @@ public class MainActivity extends AppCompatActivity {
                         .replace(R.id.fragmentcontainer, new BaseMapFragment())
                         .commit();
                 currentFragment = Fragments.BASE_MAP;
-                //TODO: find right icon to replace current one
-                racingButton.setImageResource(R.drawable.race_foreground);
+
+                resetButtons();
             }
 
         });
@@ -98,5 +107,13 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
             currentFragment = Fragments.BASE_MAP;
         } else finish();
+    }
+
+
+    public void resetButtons(){
+        //TODO: find right icon to replace current one
+        racingButton.setImageResource(R.drawable.race_foreground);
+        trainingButton.setImageResource(R.drawable.training_foreground);
+        toCharacterButton.setImageResource(R.drawable.ic_launcher_foreground);
     }
 }
